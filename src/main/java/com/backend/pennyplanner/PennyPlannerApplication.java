5d2a0dfe34e642d3d5c6e17c1ed4cb822cee235d
Vignesh Chivukula <vigneshc994@gmail.com>
2025-03-22 08:45:48 -0600

    Adding folder structure
    Adding folder structure

---
+++ src/main/java/com/backend/pennyplanner/PennyPlannerApplication.java
@@ -0,0 +1,12 @@
+package com.backend.pennyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PennyPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyPlannerApplication.class, args);
+	}
+
+}
